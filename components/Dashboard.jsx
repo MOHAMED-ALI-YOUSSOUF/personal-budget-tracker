972ca56824042dcd57bb1a309b3529510970ec8e
MOHAMED-ALI-YOUSSOUF <wizzimed@gmail.com>
2024-12-21 15:39:03 +0300

    final version

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -22,8 +22,8 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Personal Budget Tracker</h2>
+        <div className="flex items-center justify-between space-y-2  ">
+          <h2 className="text-xl font-bold tracking-tight sm:text-3xl ">Personal Budget Tracker</h2>
           <AddTransaction />
         </div>
         <Tabs value={activeTab} onValueChange={setActiveTab} className="space-y-4">
