823a4104dd2ea819cad49fd847e90b375c66fecd
MOHAMED-ALI-YOUSSOUF <wizzimed@gmail.com>
2024-12-23 17:11:03 +0300

    project completed

---
+++ app/LangSelector/page.js
@@ -0,0 +1,12 @@
+import LangSelector from "@/components/LangSelector";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <LangSelector />
+    </div>
+  );
+};
+
+export default page;
